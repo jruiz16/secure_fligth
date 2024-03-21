436956a07c2035202a3609fa52ddc5fed4dedb1e
ARUS\juan.ruiz <juan.ruiz@arus.com.co>
2024-03-21 09:14:15 -0500

    agregar front al deploy y ajuste de cors

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ kubectl create secret docker-registry regcred --docker-server=https://index.dock
 4. backend\deploy\mysql\service.yaml
 5. backend\deploy\backend\deployment.yaml
 6. backend\deploy\backend\service.yaml
+7. backend\deploy\frontend\secret.yaml
+8. backend\deploy\frontend\deployment.yaml
+9. backend\deploy\frontend\service.yaml
 
-# Comando para exponer el servicio de APP en local por el puerto 9081
-kubectl port-forward service/express-app-service 9081:8083 -n secure-flight
+# Comando para exponer el servicio de APP en local por el puerto 8083
+kubectl port-forward service/express-app-service 8083:8083 -n secure-flight
+
+# Comando para exponer el servicio de Front en local por el puerto 8085
+kubectl port-forward service/express-app-service 8085:8085 -n secure-flight
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const db = require("./src/models");
 
-let corsOptions = ["http://localhost", "http://localhost:8082"];
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (corsOptions.indexOf(origin) === -1) {
-//         return callback(
-//           new Error(`La politica de CORS para esta url no permite el acceso`),
-//           false
-//         );
-//       }
-//       return callback(null, true);
-//     },
-//     credentials: true,
-//   })
-// );
+let corsOptions = ["http://localhost:8085"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (corsOptions.indexOf(origin) === -1) {
+        return callback(
+          new Error(`La politica de CORS para esta url no permite el acceso`),
+          false
+        );
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
 
 require("./src/routes/Usuarios.routes")(app);
-const PORT = 8081;
+const PORT = 8083;
 
 db.sequelize.sync({ force: false }).then(() => {
   console.log("sequelize ..... db");
